1e9e7e7eee890f6e36f22bde653d7f7e7ef8cd47
XQY33 <xuqiyong2023@gmail.com>
2024-07-15 20:30:22 +0800

    left and right next:_check_events()

--- alien_invasion.py
+++ alien_invasion.py
@@ -8,16 +8,16 @@ class AlienInvasion:
         """初始化游戏资源"""
         pygame.init()
         self.clock = pygame.time.Clock()
-        self.settings = Settings()
+        self.settings = Settings()       #Setting类实例化
         self.screen = pygame.display.set_mode((self.settings.screen_width,self.settings.screen_height))
         pygame.display.set_caption("Alien Invasion")
-        self.bg_color = (230, 230, 230)
-        self.ship = Ship(self)
+        self.ship = Ship(self)     #传入 self到ai_game进行实例化   初始化ship图片
 
     def run_game(self):
         """开始游戏主循环"""
         while True:
             self._check_events()
+            self.ship.update()
             self._update_screen()
             self.clock.tick(60)
 
@@ -26,6 +26,18 @@ class AlienInvasion:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
+
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_RIGHT:
+                    self.ship.moving_right = True
+                elif event.key == pygame.K_LEFT:
+                    self.ship.moving_left = True
+
+            elif event.type==pygame.KEYUP:
+                if event.key == pygame.K_RIGHT:
+                    self.ship.moving_right = False
+                elif event.key == pygame.K_LEFT:
+                    self.ship.moving_left = False
 
     def _update_screen(self):
         # 每次循环时都重新绘制屏幕
--- ship.py
+++ ship.py
@@ -1,8 +1,11 @@
 import pygame
+from settings import Settings
+
 class Ship:
     def __init__(self, ai_game):
         self.screen = ai_game.screen
         self.screen_rect = ai_game.screen.get_rect()
+        self.settings = ai_game.settings    #注意此处   别忘记创建ship的传入的ai_game对象的seetings的实例化对象   ...
 
         #加载飞船图像接近外接矩形
         self.image = pygame.image.load('images/ship.bmp')
@@ -10,6 +13,24 @@ class Ship:
 
         #每艘飞船都放在底部
         self.rect.midbottom = self.screen_rect.midbottom
+        #飞船属性x中存储一个浮点数
+        self.x = float(self.rect.x)
+
+        #移动标志位
+        self.moving_right = False
+        self.moving_left = False
+
+    def update(self):
+        """根据移动标志位调整飞船位置"""
+        if self.moving_right and self.rect.right <self.screen_rect.right:
+            self.x += self.settings.ship_speed
+
+        if self.moving_left and self.rect.left > 0:
+            self.x-= self.settings.ship_speed
+        #更新对象  此处x坐标才是的对象左右移动的位置的实例化
+        self.rect.x=self.x
+
+
 
     def blitme(self):
         #在指定位置绘制飞船
